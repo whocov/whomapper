c2cf7075d4f426e45b50286da6301be68db2aef1
henryls1 <38218241+henryls1@users.noreply.github.com>
2021-05-24 15:33:03 +0200

    adding new functions to compile parts of a full map

---
+++ R/who_map_annotate.R
@@ -0,0 +1,35 @@
+#' function to add annotations to WHO map
+#' code by IZAWA, Yurie & LAURENSON-SCHAFER Henry
+#' @details function to add annotations to WHO map
+#' @param data_source - user defined data source
+#' @export
+
+
+who_map_annotate <- function(data_source) {
+
+  # 2. Data source (modify accordingly)
+
+  if (missing(data_source)) {
+    data_source <- "Data Source: World Health Organization,
+Map Production: WHO Health Emergencies Programme
+© WHO 2020. All rights reserved."
+  }
+
+
+  logo <-  pull_who_logo()
+
+  # 4. Disclaimer
+  text_disc <- "The designations employed and the presentation of the material in this publication do not imply the expression of any opinion whatsoever on the part of WHO concerning the legal status of any country, territory, city or area or of its authorities,
+or concerning the delimitation of its frontiers or boundaries. Dotted and dashed lines on maps represent approximate border lines for which there may not yet be full agreement. [1] All references to Kosovo in this document should be
+understood to be in the context of the United Nations Security Council resolution 1244 (1999). Data for Serbia and Kosovo (UNSCR 1244, 1999) have been aggregated for visualization purposes."
+
+  annotation_custom(rasterGrob(logo), xmin = 150, xmax = 180, ymin = 70, ymax = 100) +
+
+  annotate("text", x = 100, y = -70, label = text_disc, size = 3) +
+
+  annotate("text", x = 100, y = -80, label = data_source, size = 3)
+
+
+}
+
+
