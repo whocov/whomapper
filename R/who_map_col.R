c5227ba203c06f86c07983fc74667dd6421b7d79
henryls1 <38218241+henryls1@users.noreply.github.com>
2021-05-31 16:30:25 +0200

    new colour function

---
+++ R/who_map_col.R
@@ -0,0 +1,18 @@
+#' function to get default who map colours
+#' code by IZAWA, Yurie & LAURENSON-SCHAFER Henry
+#' @details function to get default who map colours
+#' @param x which colour index should be used
+#' @param new_scale - should a new scale be added?
+#' @export
+
+
+who_map_col <- function(x = "no_data") {
+
+  case_when(x == "no_data" ~ "#d2d2d2",
+            x == "not_applicable" ~ "#cccccc",
+            x == "background" ~ "#EBEBEB",
+            x == "lakes" ~ "#BEE8FF",
+            x == "border" ~ "#9c9c9c")
+
+
+}
