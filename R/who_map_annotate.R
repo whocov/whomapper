e2d7c41bf95c1ca45f7ac560ae5cb60c07877a47
henryls1 <38218241+henryls1@users.noreply.github.com>
2021-06-03 14:28:04 +0200

    added  new logo spacing for SEARO

--- R/who_map_annotate.R
+++ R/who_map_annotate.R
@@ -25,14 +25,26 @@ who_map_annotate <- function(region = "HQ", data_source) {
                concerning the legal status of any country, territory, city or area or of its authorities, or concerning the delimitation of its frontiers or boundaries.
                Dotted and dashed lines on maps represent approximate border lines for which there may not yet be full agreement")
 
-  # list(
-  #   annotation_custom(rasterGrob(logo), xmin = 150, xmax = 180, ymin = 70, ymax = 100),
-  #   annotate("text", x = -60, y = -70, label = text_disc, size = 3, hjust = 0),
-  #   annotate("text", x = -40, y = -80, label = data_source, size = 3, hjust = 0)
-  # )
+  y_logo <- case_when(
+    grepl("hq|global", region, ignore.case = T) ~ "WHO_logo.png",
+    grepl("^sea", region, ignore.case = T) ~ "SEARO_logo.png",
+    TRUE ~ NA_character_
+  )
+
+  if (grepl("hq|global", region, ignore.case = T)) {
+    logo_y <- 1.06
+    logo_x <- 0.91
+    logo_width <- 0.18
+  } else if (grepl("^sea", region, ignore.case = T)) {
+    logo_y <- 1.07
+    logo_x <- 0.92
+    logo_width <- 0.16
+  }
 
   list(
-    annotation_custom(grobTree(rasterGrob(logo, x = unit(0.91, "npc"), y = unit(1.06,"npc"), width = 0.18))),
+    annotation_custom(grobTree(rasterGrob(
+      logo, x = unit(logo_x, "npc"), y = unit(logo_y,"npc"), width = logo_width)
+    )),
     # annotation_custom(rasterGrob(logo), xmin = 125, xmax = 175, ymin = 70, ymax = 100),
     labs(caption = glue::glue("{text_disc}\n\n{data_source}"))
   )
