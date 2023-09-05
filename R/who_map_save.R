62aa332fd631d340c83d625dd49167595e1d7b21
Henry Laurenson-Schafer <henry.laurenson.schafer@gmail.com>
2023-09-05 14:44:33 +0200

    update to who_map_save

--- R/who_map_save.R
+++ R/who_map_save.R
@@ -3,11 +3,13 @@
 #' @details wrapper function around ggsave to save map in correct dimensions
 #' @param name the name of the plot to be saved
 #' @param plot the plot object to be saved. defaults to ggplot2::last_plot()
+#' @param width width of the plot. Defaults to 11.8in for world maps
+#' @param height height of the plot. Defaults to 7.2in for world maps
 #' @export
 
 
-who_map_save <- function(name, plot = ggplot2::last_plot(), dpi = 800, ...) {
+who_map_save <- function(name, plot = ggplot2::last_plot(), width = 11.8, height = 7.2, dpi = 800, ...) {
 
-  ggsave(name, plot, width = 300, height = 183, units = "mm", dpi = dpi, ...)
+  ggsave(name, plot, width = width, height = height, dpi = dpi, ...)
 
 }
